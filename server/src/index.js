d5add874aea2113e1d0347a1011c4e7ab4fef3ff
Aryan9901 <911aaryan@gmail.com>
2024-07-24 20:57:03 +0530

    exported app

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,22 @@
 const dotenv = require("dotenv");
 const connectDB = require("./db/index.js");
 const app = require("./app");
+
 dotenv.config({
 	path: "./.env",
 });
 
-connectDB()
-	.then(() => {
+const startServer = async () => {
+	try {
+		await connectDB();
 		app.listen(process.env.PORT || 8000, () => {
-			console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+			console.log(`⚙️  Server is running at port: ${process.env.PORT}`);
 		});
-	})
-	.catch((err) => {
+	} catch (err) {
 		console.log("MONGO db connection failed !!! ", err);
-	});
+	}
+};
+
+startServer();
+
+module.exports = app; // Export the app for Vercel
